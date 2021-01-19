b92b894685e6eca0fd28ab5666086d42798ab786
dxdiadora <74500316+dxdiadora@users.noreply.github.com>
2021-01-19 16:44:36 +0200

    Update Trie.h

--- Trie.h
+++ Trie.h
@@ -12,7 +12,20 @@ typedef struct node {
 
 //void lower_str(char * word, int size);
 //int get_word(char * word); // scan a word and return size of the scanned word
+int get_word(char word[]);
 
+void lower_str(char * word, int size);
+
+void insert_word(node * root, char * word, int size);
+
+void printTrie(node* head,char word[], int pos);
+
+int maxWordLength(node * tNode, int counter);
+
+void cleanNode(struct node * root);
+
+
+void printrevTrie(node* head,char word[], int pos);
 
 
 #endif //EX4_TRIE_H
